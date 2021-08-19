50557f002bbe74dad495ae5724a4df47b854f75a
beartato327 <rcapurro@unomaha.edu>
2021-08-19 15:29:57 -0500

    .env challenge

--- myApp.js
+++ myApp.js
@@ -1,22 +1,28 @@
 var express = require('express');
 var app = express();
+require('dotenv').config()
 
+console.log("Hello World");
+const mySecret = process.env.MESSAGE_STYLE
+absolutePath = __dirname + '/views/index.html';
+var response = 'Hello json'
+app.use("/public", express.static(__dirname + '/public'))
 
+ console.log(mySecret);
 
+app.get('/json', (req, res) => {
+    console.log(process.env);
+  if(process.env.MESSAGE_STYLE === "uppercase"){
+      response = response.toUpperCase();
+    }
+  res.json({
+   "message":response
+  })
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/',(req,res) => {
+  res.sendFile(absolutePath);
+});
 
 
 
